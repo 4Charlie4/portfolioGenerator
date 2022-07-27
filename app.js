972e4af17b8118c4c15532e172e21e1a3b689772
Charles Duran <62945929+4Charlie4@users.noreply.github.com>
2022-07-27 17:25:50 -0400

    Generate html page with user input

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
+const fs = require("fs");
+const generatePage = require("./src/pageTemplate.js");
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const printProfData = (profileDataArr) => {
-  for (let i = 0; i < profileDataArr.length; i++) {
-    console.log(profileDataArr[i]);
-  }
-  console.log("============");
-  //this functions the same as code above
-  profileDataArgs.forEach(profileItem => console.log(profileItem));
-};
+const [name, github] = profileDataArgs;
 
-printProfData(profileDataArgs);
+fs.writeFile("./index.html", generatePage(name, github), (err) => {
+  if (err) throw new error(err);
+
+  console.log("Portfolio Generated! Check index.html.");
+});
